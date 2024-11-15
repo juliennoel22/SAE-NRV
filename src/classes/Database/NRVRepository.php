7f13a7134d9f9871b80637290d258863e66b4b23
Slophil <slophil.contact@gmail.Com>
2024-11-15 11:17:18 +0100

    update

--- src/classes/Database/NRVRepository.php
+++ src/classes/Database/NRVRepository.php
@@ -90,7 +90,11 @@ class NRVRepository
 
     public function getAllSpectacles(): bool|array
     {
-        $query = "SELECT spectacle.spectacle_titre AS spectacle_titre, soiree.soiree_date AS spectacle_date, spectacle.spectacle_horaire AS spectacle_horaire, image.image_url AS image_spectacle_url
+        $query = "SELECT
+                        spectacle.*,
+                        soiree.soiree_date AS spectacle_date,
+                        spectacle.spectacle_horaire AS spectacle_horaire,
+                        image.image_url AS image_spectacle_url
                 FROM spectacle
                 JOIN soiree ON spectacle.spectacle_soiree_id = soiree.soiree_id
                 LEFT JOIN image ON spectacle.spectacle_id = image.image_spectacle_id";
